bd224f05d7f282f95ece6e40902fa1e23c414640
Ahmed Nasir Latif <freesurf264@gmail.com>
2024-12-25 13:21:14 +0500

    Update main.cpp
    Combine all classes into main file.. Add a Authentication module. Check the main function.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,103 @@
 #include <iostream>
 #include <string>
-#include "Book.h"
-#include "User.h"
-#include "Member.h"
-#include "Student.h"
+#define MAXBOOKS 100
+#define MAX_MEMBERS 50
 using namespace std;
 
+class Authentication
+{
+private:
+    string username;
+    string password;
+
+public:
+    void setPassword();
+    string getUserName();
+    string getPassWord();
+};
+string Authentication::getUserName(){
+     return username;
+}
+string Authentication::getPassWord(){
+  return password;
+}
+void Authentication::setPassword()
+{
+    cout << "Enter your username: ";
+    cin.ignore();
+    getline(cin, username);
+    cout << "Enter your password: ";
+    cin.ignore();
+    getline(cin, password);
+}
+
+class Book
+{
+protected:
+    unsigned int bookId;
+    string title;
+    string author;
+    bool isAvailable;
+
+public:
+    void addBook(int);
+    void displayBooks();
+    int getBookId();
+};
+class User
+{
+protected:
+    // int userId;
+    string name;
+
+public:
+    void viewBooks();
+    void setter();
+    void getter();
+};
+class Member : virtual public User
+{
+protected:
+    int memberId;
+
+public:
+    void addMem(int);
+    void dispMem();
+    int getMemId()
+    {
+        return memberId;
+    }
+};
+class Student : virtual public User
+{
+public:
+    // void viewBooks();
+};
+class Librarian : virtual public User
+{
+protected:
+    Book books[MAXBOOKS];
+    Member members[MAX_MEMBERS];
+    int bookCount;
+    int memCount;
+
+public:
+    void addBook();
+    void appBook();
+    // void updateBook();
+    void delBook();
+    // void genRepBook();
+    int searchBook(int &, int &);
+    void dispBooks();
+    void addMember();
+    // void delMember();
+    void displayMember();
+    int searchMember(int, int);
+    int getBookCount()
+    {
+        return bookCount;
+    }
+};
 void Book::displayBooks()
 {
     cout << "Book Id: " << bookId << " - " << "Book Name: " << title << " - " << "Author: " << author << endl;
@@ -29,6 +121,7 @@ void Book::addBook(int id)
 void User::setter()
 {
     cout << "Enter your name: ";
+    cin.ignore();
     getline(cin, name);
 }
 void User::getter()
@@ -44,7 +137,7 @@ void Member::addMem(int id)
 void Member::dispMem()
 {
     User::getter();
-    cout << "Member Id: " << memberID << endl;
+    cout << "Member Id: " << memberId << endl;
 }
 
 void Librarian::dispBooks()
@@ -80,16 +173,17 @@ void Librarian::addMember()
 }
 void Librarian::displayMember()
 {
-    for (int i = 0; i < memCount; i++){
+    for (int i = 0; i < memCount; i++)
+    {
         members[i].dispMem();
     }
 }
-void Librarian::searchMember(int id, int nn)
+int Librarian::searchMember(int id, int nn)
 {
     int s;
     for (int i = 0; i < nn; i++)
     {
-        if (id == getMemId())
+        if (id == members[i].getMemId())
         {
             s = i;
             break;
@@ -172,7 +266,101 @@ int Librarian::searchBook(int &id, int &nn)
 }
 int main()
 {
-    Librarian l1;
-    l1.addBook();
-    l1.dispBooks();
+    Librarian library;
+    Authentication *aptr[2];
+    int userCount = 0;
+    int ch;
+    do
+    {
+        cout << "----- Library Management System -------- " << endl
+             << "1. Create Account" << endl
+             << "2. Login" << endl
+             << "3. Search a Book" << endl
+             << "4. Display Books" << endl
+             << "5. Exit" << endl
+             << "Enter your choice: ";
+        cin >> ch;
+        switch (ch)
+        {
+        case 1:
+        {
+            if (userCount < 2)
+            {
+                aptr[userCount] = new Authentication;
+                aptr[userCount]->setPassword();
+                userCount++;
+            }
+            else
+            {
+                cout << "Can't add more admins. " << endl;
+            }
+        }
+        break;
+        case 2:
+        {
+            string id, pass;
+            cout << "Enter your user id: ";
+            cin.ignore();
+            getline(cin, id);
+            for (int i = 0; i < userCount; i++)
+            {
+                if (aptr[i]->getUserName() == id)
+                {
+                    int j = 0;
+                    while (j < 3)
+                    {
+                        cout << "Enter your password: ";
+                        cin.ignore();
+                        getline(cin, pass);
+                        if (pass == aptr[i]->getPassWord())
+                        {
+                            cout << "You sucessfully login. " << endl;
+                            cout << "---- LMS ------" << endl
+                                 << "1. Add a Book" << endl
+                                 << "2. Search a Book" << endl
+                                 << "3. Append a Book" << endl
+                                 << "4. Delete a Book" << endl
+                                 << "5. Display Books" << endl
+                                 << "6. Add a Member" << endl
+                                 << "7. Search a Member" << endl
+                                 << "8. Delete a Member" << endl
+                                 << "9. Display Members" << endl
+                                 << "10. Exit" << endl
+                                 << "Enter your choice: ";
+                            cin >> ch;
+                        }
+                        else
+                        {
+                            cout << "Password not match. " << endl;
+                        }
+                        j++;
+                    }
+                    exit(1);
+                }
+                else
+                {
+                    cout << "User name not found. " << endl;
+                    break;
+                }
+            }
+        }
+        break;
+        case 3:
+        {
+            int id;
+            cout << "Enter the book id: ";
+            cin >> id;
+            // library.searchBook(id, getBookCount());
+        }
+        break;
+        case 4:
+        {
+            library.dispBooks();
+        }
+        break;
+        case 5:
+            break;
+        }
+    } while (ch != 5);
 }
+
